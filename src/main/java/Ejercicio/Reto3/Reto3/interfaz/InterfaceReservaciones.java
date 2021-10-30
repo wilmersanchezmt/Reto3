6fc1ad740378f719bd793f85db08a9cb40fc2674
wilmer-sanchez <wilmerandressr@hotmail.com>
2021-10-30 13:07:24 -0500

    Reto 3 Definitivo

---
+++ src/main/java/Ejercicio/Reto3/Reto3/interfaz/InterfaceReservaciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Ejercicio.Reto3.Reto3.interfaz;
+
+import Ejercicio.Reto3.Reto3.modelo.Reservaciones;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface InterfaceReservaciones extends CrudRepository<Reservaciones,Integer>{
+    
+}
